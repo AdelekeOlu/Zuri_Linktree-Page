2f403d766669e2a4b3ae4d21ca1d51f699a5a5a6
AdelekeOlu <adeleke.olukunle@yahoo.com>
2022-10-26 13:03:39 +0100

    Added New Links
    links for python books, background check for coders and design books

--- src/App.js
+++ src/App.js
@@ -15,6 +15,9 @@ function App() {
       <a href="https://twitter.com/dat_leke" target="_blank" rel='noreferrer' id='twitter'>Twitter Link</a>
       <a href="https://training.zuri.team/" target="_blank" rel='noreferrer' id='btn__zuri'>Zuri Team</a>
       <a href="https://books.zuri.team/" target="_blank" rel='noreferrer' id='books'>Zuri Books</a>
+      <a href="https://books.zuri.team/python-for-beginners?ref_id=<Adeleke Olukunle>" target="_blank" rel='noreferrer' id='book__python'>Python Books</a>
+      <a href="https://background.zuri.team" target="_blank" rel='noreferrer' id='pitch'>Background Check for Coders</a>
+      <a href="https://books.zuri.team/design-rules" target="_blank" rel='noreferrer' id='book__design'>Design Books</a>
       </div>
       
     </div>
