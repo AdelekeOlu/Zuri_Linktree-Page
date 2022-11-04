26ea816d8ece88e1fa37dc81e0f1489d453516b1
AdelekeOlu <adeleke.olukunle@yahoo.com>
2022-11-04 12:20:23 +0100

    feat: created a Contact.jsx file

---
+++ src/pages/contact/Contact.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../contact/contact.scss'
+
+const Contact = () => {
+  return (
+    <div>
+        <form action="">
+            <h1>Contact Me</h1>
+        </form>
+    </div>
+  )
+}
+
+export default Contact
