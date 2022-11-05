12aac2c8e3ff3c8ae00b37e5175ab985bc81ffec
AdelekeOlu <adeleke.olukunle@yahoo.com>
2022-11-05 11:28:37 +0100

    feat: added labels and inputs for last name
    and first name

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -3,9 +3,23 @@ import '../contact/contact.scss'
 
 const Contact = () => {
   return (
-    <div>
-        <form action="">
+    <div className="contact">
+        <form>
+            <div className='title'>
             <h1>Contact Me</h1>
+            <p>Hi there, contact me to ask me about anything you have in mind.</p>
+            </div>
+
+            <div className="names">
+                <div className="firstname">
+                    <label>First name</label> <br />
+                    <input type="text" placeholder='Enter your first name' id='first_name'/>
+                    </div>
+                <div className="lastname">
+                    <label>Last name</label> <br />
+                    <input type="text" placeholder='Enter your last name' id='last_name'/>
+                </div>
+            </div>
         </form>
     </div>
   )
