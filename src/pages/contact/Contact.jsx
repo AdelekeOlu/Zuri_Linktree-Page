4d0a537ec2e2d167739a4a52d08e0d03ef0e921d
AdelekeOlu <adeleke.olukunle@yahoo.com>
2022-11-05 13:37:29 +0100

    feat:added a submit function

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import '../contact/contact.scss'
 
 const Contact = () => {
+    function handleSubmit(e) {
+        e.preventDefault();
+        alert("Message Sent")
+    }
   return (
     <div className="contact">
-        <form>
+        <form onSubmit={handleSubmit}>
             <div className='title'>
             <h1>Contact Me</h1>
             <p>Hi there, contact me to ask me about anything you have in mind.</p>
@@ -13,17 +17,31 @@ const Contact = () => {
             <div className="names">
                 <div className="firstname">
                     <label>First name</label> <br />
-                    <input type="text" placeholder='Enter your first name' id='first_name'/>
+                    <input type="text" placeholder='Enter your first name' id='first_name' required/>
                     </div>
                 <div className="lastname">
                     <label>Last name</label> <br />
-                    <input type="text" placeholder='Enter your last name' id='last_name'/>
+                    <input type="text" placeholder='Enter your last name' id='last_name' required/>
                 </div>
             </div>
 
             <div className="allemail">
                 <label>Email</label>
-                <input type="email" placeholder='youremail@email.com' id='email'/>
+                <input type="email" placeholder='youremail@email.com' id='email' required/>
+            </div>
+
+            <div className="allmessage">
+                <label>Message</label>
+                <textarea name="" id="message" cols="30" rows="10" placeholder='Send me a message and I will reply you as soon as possible...' required></textarea>
+            </div>
+
+            <div className="checkbox">
+                <input type="checkbox" required/> 
+                <p>You agree to providing your data to Adeleke who may contact you</p>
+            </div>
+
+            <div className="allbutton">
+            <button type='submit' id='btn__submit'>Send Message</button>
             </div>
         </form>
     </div>
